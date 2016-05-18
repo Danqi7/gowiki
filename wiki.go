857db00bcce5e84270ce24600a26f53aa2cb0ac5
Danqi Liao <danqiliao@gmail.com>
2016-05-18 16:54:57 -0500

    add template

--- wiki.go
+++ wiki.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 )
 
 type Page struct{
@@ -24,10 +25,34 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func main(){
-	p1 := &Page{Title: "TetsPage", Body: []byte("This is a sample page.")}
-	p1.save()
-	p2, _ := loadPage("TetsPage")
-	fmt.Println(string(p2.Body))
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w,p)
+
 }
+
+func viewHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil{
+		p = &Page{Title: title}
+	}
+	
+	renderTemplate(w, "edit", p)
+}
+
+
+func main(){
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+//	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
 
